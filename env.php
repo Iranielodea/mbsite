bb8e10d4fc7c6b6d8287d642abc8a363bd2eae1e
Iranielodea <65976595+Iranielodea@users.noreply.github.com>
2020-11-23 22:02:32 -0300

    Update env.php

--- env.php
+++ env.php
@@ -4,14 +4,13 @@ putenv('DISPLAY_ERRORS_DETAILS='.true);
 
 //http://www.irasoft.com.br/teste/index.php/grupo
 
-putenv('MB_MYSQL_HOST=mysql.irasoft.com.br');
-putenv('MB_MYSQL_DBNAME=irasoft');
-putenv('MB_MYSQL_USER=irasoft');
-putenv('MB_MYSQL_PASSWORD=tioIra70');
-putenv('MB_MYSQL_PORT=80');
+putenv('MB_MYSQL_HOST=
+putenv('MB_MYSQL_DBNAME=
+putenv('MB_MYSQL_PASSWORD=senha
+putenv('MB_MYSQL_PORT=9595
 
 // putenv('MB_MYSQL_HOST=localhost');
-// putenv('MB_MYSQL_DBNAME=mb');
+// putenv('MB_MYSQL_DNAME=mb')
 // putenv('MB_MYSQL_USER=root');
 // putenv('MB_MYSQL_PASSWORD=');
 // putenv('MB_MYSQL_PORT=80');
